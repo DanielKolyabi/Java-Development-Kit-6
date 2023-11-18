eb327b68b581c4a6595612a9da1d1e254a3dd552
ShuMAhd <shuman@zykaika.ru>
2023-11-18 22:12:44 +0800

    Добавил вывод в консоль, выиграл или проиграл игрок.

--- src/main/java/com/example/MontyHallGame.java
+++ src/main/java/com/example/MontyHallGame.java
@@ -26,10 +26,36 @@ public class MontyHallGame {
     System.out.println("Негативные результаты: " + negativeResults);
     System.out.println("Процент позитивных результатов: " + positivePercentage + "%");
 
-    // Вывод ходов игр
+// Вывод ходов игр
     for (int i = 1; i <= TOTAL_TRIALS; i++) {
-      System.out.println("Ходы игры " + i + ": " + moves.get(i));
+      List<Integer> gameMoves = moves.get(i);
+
+      // Проверка, чтобы избежать ошибок, если что-то пошло не так
+      if (gameMoves != null && gameMoves.size() >= 3) {
+        int playerChoice = gameMoves.get(0);
+        int goatDoor = gameMoves.get(1);
+        int switchChoice = gameMoves.get(2);
+
+        // Получение результата для текущей игры
+        boolean win = results.get(i);
+
+        // Формирование строки с пояснением
+        String explanation = "Игрок выбрал дверь " + playerChoice + ", " +
+            "Ведущий открывает дверь " + goatDoor + ", за которой козёл, " +
+            (switchChoice > 0 ?
+                "Игрок решает выбрать дверь " + switchChoice :
+                "Игрок решает оставить выбранную дверь.") +
+            (win ? " Игрок выйграл, за дверью авто! " : " Игрок проиграл, за дверью козёл! ");
+
+        // Вывод строки с пояснением в консоль
+        System.out.println("Ход игры " + i + ": " + explanation);
+      } else {
+        // Если что-то пошло не так, выведем сообщение об ошибке
+        System.out.println("Ошибка при обработке хода игры " + i);
+      }
     }
+
+
   }
 
   private static boolean playMontyHallGame(Map<Integer, List<Integer>> moves, int gameNumber) {
